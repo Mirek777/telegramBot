282d7f994bc96d06ad4f389763d8d29814b8f92c
Mirek777 <s-a-n-1@mail.ru>
2022-02-27 20:09:04 +0300

    2

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ name = ''
 surname = ''
 city = ''
 quantityVacancies = 4000
+technology = ''
 textQuantityVacancies = '' + 'базе ' + str(quantityVacancies) + ' вакансий'
 
 bot = telebot.TeleBot("5100500044:AAESD-UOYikG3MH8JRv_vdOfuoxZzh3_34I")
@@ -57,6 +58,13 @@ def reg_experience(message):
     keyboard.add(key_senior)
     question = "Какую позицию ты рассматриваешь (опыт работы)?"
     bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
+    bot.register_next_step_handler(message, reg_techno)
+
+def reg_techno(message):
+    bot.send_message(message.from_user.id, 'Напиши ключевые технологии, которыми владеешь')
+    global technology
+    technology = message.text
+    bot.send_message(message.from_user.id, 'Отлично. Сейчас поищем результат для ' + technology)
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
@@ -66,7 +74,14 @@ def callback_worker(call):
     elif call.data == "remote":
         # пересчет
         bot.send_message(call.message.chat.id, 'Сейчас в ' + textQuantityVacancies + ' удаленной работы')
-
-
+    elif call.data == "junior":
+        # пересчет
+        bot.send_message(call.message.chat.id, 'Сейчас в ' + textQuantityVacancies + ' для junior-разработчика')
+    elif call.data == "middle":
+        # пересчет
+        bot.send_message(call.message.chat.id, 'Сейчас в ' + textQuantityVacancies + ' для middle-разработчика')
+    elif call.data == "senior":
+        # пересчет
+        bot.send_message(call.message.chat.id, 'Сейчас в ' + textQuantityVacancies + ' для senior-разработчика')
 
 bot.infinity_polling()
